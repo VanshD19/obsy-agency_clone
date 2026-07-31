ea2b54a1214e112411de00953baf4daa39c980f2
Vansh Dhar <vanshdhar.jk01@gmail.com>
2026-07-31 12:45:16 +0530

    created loader

---
+++ vairiable.js
@@ -0,0 +1,10 @@
+var timelime_1 = gsap.timeline();
+var timelime_wait_dots = gsap.timeline({
+    repeat:-1,
+    opacity:0,
+});
+var loader_lines = document.querySelectorAll(".h1");
+var counter = document.querySelector("#counter");
+
+
+
